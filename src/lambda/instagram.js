f07ee801ad369ed9dad5a974141be8687e26150f
Jon-Kyle Mohr <contact@jon-kyle.com>
2019-03-27 17:24:00 -0700

    insta cache

--- src/lambda/instagram.js
+++ src/lambda/instagram.js
@@ -2,24 +2,25 @@ import FileCookieStore from 'tough-cookie-filestore2'
 import Instagram from 'instagram-web-api'
 import dotenv from 'dotenv'
 import dayjs from 'dayjs'
+import fs from 'fs'
+
 dotenv.config()
 
 const username = process.env.IG_USERNAME
 const password = process.env.IG_PASSWORD
 const cookieStore = new FileCookieStore('/tmp/cookies.json')
 const client = new Instagram({ username, password, cookieStore })
-
-let state = { }
-let cached
+const CACHE_PATH = '/tmp/instagram.json'
 
 export async function handler (event, context) {
-  if (!event.queryStringParameters.cache && isCached()) {
+  if (fs.existsSync(CACHE_PATH) && !event.queryStringParameters.cache && isCached()) {
+    const state = JSON.parse(fs.readFileSync(CACHE_PATH))
     console.log('instagram is cached')
     return {
       statusCode: 200,
       contentType: 'json',
       headers: { 'content-type': 'application/json; charset=utf-8' },
-      body: JSON.stringify(state),
+      body: JSON.stringify(state.posts),
     }
   }
 
@@ -34,15 +35,14 @@ export async function handler (event, context) {
       caption: edge.node.accessibility_caption
     }))
 
+    fs.writeFileSync(CACHE_PATH, JSON.stringify({ date: dayjs().format('YYYY-MM-DD'), posts }))
     console.log('Succesfully fetched instagram')
-    cached = dayjs()
-    state = posts
 
     return {
       statusCode: 200,
       contentType: 'json',
       headers: { 'content-type': 'application/json; charset=utf-8' },
-      body: JSON.stringify(state)
+      body: JSON.stringify(posts)
     }
   } catch (err) {
     return {
@@ -58,6 +58,11 @@ export async function handler (event, context) {
  * Is Cached
  */
 function isCached () {
-  const now = dayjs()
-  return cached && dayjs(cached).add(1, 'day').isAfter(now)
+  if (fs.existsSync(CACHE_PATH)) {
+    const now = dayjs()
+    const cached = JSON.parse(fs.readFileSync(CACHE_PATH, 'utf8'))
+    return cached && cached.date && dayjs(cached.date).add(1, 'day').isAfter(now)
+  } else {
+    return false
+  }
 }
